4cccb6727e1de278608325fda8de59f0d23f7d38
Olivier Winter <owinter@popeye-login2.cm.cluster>
2024-05-07 03:29:01 -0700

    overwrite parameter for jobs

--- pykilosort/create_jobs.py
+++ pykilosort/create_jobs.py
@@ -11,25 +11,21 @@ substitutions = {
     'LOG_PATH': str(doc_path.joinpath("logs")),
     'TIME': "16:00:00",
     'PID': None,
-    'MAIL_USER': 'cal2254@columbia.edu'
+    'MAIL_USER': 'olivier.winter@internationalbrainlab.org'
 }
 
 pids = [
-    'bf827e50-eed3-4984-908e-c48ffbeae7b5',
-    'c07d13ed-e387-4457-8e33-1d16aed3fd92',
-    'c0c3c95d-43c3-4e30-9ce7-0519d0473911',
-    'c16b8e98-100f-40ce-b417-b1524c7e1270',
-    'c4f6665f-8be5-476b-a6e8-d81eeae9279d',
-    'c5b9e063-f640-4936-b851-f7602cb6659b',
-    'c6ba6f8e-c13e-410f-b7df-e193ba0d239d',
+    '00a824c0-e060-495f-9ebc-79c82fef4c67',
 ]
 
+OVERWRITE = False
 for pid in pids:
     substitutions['PID'] = pid
     job_fn = jobs_path / f"pykilosort_{pid}.sbatch"
-    # if job_fn.exists():
-    #     print(f"Job file already exists: {job_fn}")
-    #     continue
+    if job_fn.exists():
+        if not OVERWRITE:
+            print(f"Job file already exists: {job_fn}")
+            continue
     with open(slurm_template, "r") as template:
         fdata = template.read()
     for k, v in substitutions.items():
